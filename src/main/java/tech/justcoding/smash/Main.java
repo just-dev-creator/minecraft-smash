ddf1535af56aae6c60197bc6eb2cb2bb6f665c6d
Justus Alvermann <justus@justus-alvermann.de>
2021-10-30 21:34:12 +0200

    Registred listeners & commands, loading worlds on start

--- src/main/java/tech/justcoding/smash/Main.java
+++ src/main/java/tech/justcoding/smash/Main.java
@@ -2,10 +2,22 @@ package tech.justcoding.smash;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.WorldCreator;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import tech.justcoding.smash.game.ChangeWorldCommand;
 import tech.justcoding.smash.game.GameStateManager;
+import tech.justcoding.smash.game.GriefPreventor;
+import tech.justcoding.smash.ingame.GameStart;
+import tech.justcoding.smash.ingame.Knockback;
+import tech.justcoding.smash.ingame.SetSpawnCommand;
 import tech.justcoding.smash.lobby.PlayerJoinListener;
+import tech.justcoding.smash.lobby.ResetGameCommand;
+import tech.justcoding.smash.lobby.StartCommand;
+import tech.justcoding.smash.utils.Config;
+import tech.justcoding.smash.utils.Utils;
+
+import java.util.Random;
 
 public final class Main extends JavaPlugin {
 
@@ -16,18 +28,28 @@ public final class Main extends JavaPlugin {
             Bukkit.getLogger().warning(Main.getErrorPrefix() +
                     "The plugin will be unloaded for your own safety.");
             Bukkit.getPluginManager().disablePlugin(this);
+            return;
         }
-        GameStateManager.gameState = GameStateManager.GameState.LOBBY;
+        loadWorlds();
         registerListeners();
         registerCommands();
+        Config.registerConfiguration();
+        GameStateManager.gameState = GameStateManager.GameState.LOBBY;
     }
 
     private void registerListeners() {
         PluginManager pluginManager = Bukkit.getPluginManager();
         pluginManager.registerEvents(new PlayerJoinListener(), this);
+        pluginManager.registerEvents(new GameStart(), this);
+        pluginManager.registerEvents(new GriefPreventor(), this);
+        pluginManager.registerEvents(new Knockback(), this);
     }
 
     private void registerCommands() {
+        getCommand("start").setExecutor(new StartCommand());
+        getCommand("setloc").setExecutor(new SetSpawnCommand());
+        getCommand("changeworld").setExecutor(new ChangeWorldCommand());
+        getCommand("rsgame").setExecutor(new ResetGameCommand());
     }
 
     @Override
@@ -37,14 +59,19 @@ public final class Main extends JavaPlugin {
     }
 
     private boolean isServerReady() {
-        if (Bukkit.getWorld("lobby") == null) {
+        if (!Utils.doesWorldFileExist("lobby")) {
             Bukkit.getLogger().severe(getErrorPrefix() + "Lobby world is missing!");
             return false;
-        } else if (Bukkit.getWorld("game") == null) {
+        } else if (!Utils.doesWorldFileExist("game")) {
             Bukkit.getLogger().severe(getErrorPrefix() + "Game world is missing!");
             return false;
         }
         return true;
+    }
+
+    private void loadWorlds() {
+        new WorldCreator("lobby").createWorld();
+        new WorldCreator("game").createWorld();
     }
 
     public static String getPrefix() {
@@ -57,9 +84,10 @@ public final class Main extends JavaPlugin {
         return ChatColor.DARK_GRAY + "┃ " + ChatColor.DARK_RED + "ERROR" + ChatColor.DARK_GRAY + " » " + ChatColor.GRAY;
     }
     public static String getNoPermission() {
-        return getErrorPrefix() + "Du hast nicht die benötigten Berechtigungen für den Befehl. ";
+        return getErrorPrefix() + "You do not have the required permissions for the command. ";
     }
     public static String getNoPlayer() {
-        return getErrorPrefix() + "Du musst ein Spieler sein, um diese Aktion auszuführen. ";
+        return getErrorPrefix() + "You must be a player to perform this action. ";
     }
+    public static final Random random = new Random();
 }
