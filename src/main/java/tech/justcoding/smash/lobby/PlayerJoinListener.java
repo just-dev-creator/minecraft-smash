b4ba56697626fe97464c3ed4f78b52d185a2adad
Justus Alvermann <justus@justus-alvermann.de>
2021-10-30 15:51:51 +0200

    Teleport player to lobby if game hasnt started yet

---
+++ src/main/java/tech/justcoding/smash/lobby/PlayerJoinListener.java
@@ -0,0 +1,16 @@
+package tech.justcoding.smash.lobby;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import tech.justcoding.smash.game.GameStateManager;
+
+public class PlayerJoinListener implements Listener {
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        if (GameStateManager.gameState == GameStateManager.GameState.LOBBY) {
+            event.getPlayer().teleport(Bukkit.getWorld("lobby").getSpawnLocation());
+        }
+    }
+}
